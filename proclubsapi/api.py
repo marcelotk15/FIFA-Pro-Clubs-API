125bebeb739a1063d6f253f6469675c1b940d86c
Marcelo Oliveira <marcelotk_15@hotmail.com>
2019-06-30 11:59:03 -0300

    some refactor

--- proclubsapi/api.py
+++ proclubsapi/api.py
@@ -5,44 +5,41 @@ import json
 import array as arr
 import pprint
 
-
-class __SearchResultException(Exception):
-    pass
-
-
-class __PlataformException(Exception):
-    pass
+API_URL = 'https://www.easports.com/iframe/fifa17proclubs/api/platforms/'
+PLATAFORMS = ['PC', 'XBOXONE', 'PS4']
+INFOS = ['info', 'stats', 'matches', 'membersComplete', 'seasonRank', 'seasonalStats']
 
 
 class ProClubs():
-    def __init__(self, plataform=None):
-        self.__plataform = plataform
+    def __init__(self, plataform):
+        if plataform.upper() in PLATAFORMS:
+            self.__plataform = plataform
+        else:
+            raise Exception('Wrong plataform...')
+
         self.clubs = []
         self.club = {}
 
-    def searchClubsByName(self, name=None):
+    def searchClubsByName(self, name):
         clubs = self.__get('/clubsComplete/{}'.format(name))
-
         for c in clubs:
-            self.clubs.append(clubs[c])
+            self.clubs.append(clubs[c])    
 
-    def getClub(self, key=None, infos=['info', 'stats', 'matches', 'membersComplete', 'seasonRank', 'seasonalStats']):
-        for info in infos:
-            g = self.__get('/clubs/{}/{}'.format(key, info))
+    def getClub(self, key, infos=INFOS):
+        for i in infos:
+            g = self.__get('/clubs/{}/{}'.format(key, i))
 
-            if info == 'stats' or info == 'seasonRank' or info == 'seasonalStats':
-                self.club[info] = g['{}'.format(key)]
-            elif info == 'info':
-                self.club[info] = g[0]
+            if (i == 'stats') or (i == 'seasonRank') or (i == 'seasonalStats'):
+                self.club[i] = g['{}'.format(key)]
+            elif (i == 'info'):
+                self.club[i] = g[0]
             else:
-                self.club[info] = g
+                self.club[i] = g
 
     def __get(self, endpoint=None):
-        API_URL = 'https://www.easports.com/iframe/fifa17proclubs/api/platforms/'
-        
-        return requests.get(API_URL+self.__plataform+endpoint).json()['raw']
+        return requests.get('{}{}{}'.format(API_URL, self.__plataform, endpoint)).json()['raw']
 
 
-pC = ProClubs('XBOXONE')
-pC.getClub(40518)
-pprint.pprint(pC.club)
+pC = ProClubs('xboxone')
+pC.searchClubsByName('legends')
+pprint.pprint(pC.clubs)
