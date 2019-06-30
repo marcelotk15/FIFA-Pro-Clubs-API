1eb77c954ee525dac4fb9e271c0d0b2044acd82a
Marcelo Oliveira <marcelotk_15@hotmail.com>
2019-06-30 01:16:11 -0300

    more infos about team

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![StyleCI](https://github.styleci.io/repos/194468863/shield?branch=master)](https://github.styleci.io/repos/194468863)
 # FIFA-Pro-Clubs-API
--- proclubsapi/api.py
+++ proclubsapi/api.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import array as arr
+import pprint
 
 
 class __SearchResultException(Exception):
@@ -17,7 +18,7 @@ class ProClubs():
     def __init__(self, plataform=None):
         self.__plataform = plataform
         self.clubs = []
-        self.clubInfo = []
+        self.club = {}
 
     def searchClubsByName(self, name=None):
         clubs = self.__get('/clubsComplete/{}'.format(name))
@@ -25,15 +26,23 @@ class ProClubs():
         for c in clubs:
             self.clubs.append(clubs[c])
 
-    def getClub(self, key=None):
-        self.clubInfo = self.__get('/clubs/{}/info'.format(key))
-        print(self.clubInfo)
+    def getClub(self, key=None, infos=['info', 'stats', 'matches', 'membersComplete', 'seasonRank', 'seasonalStats']):
+        for info in infos:
+            g = self.__get('/clubs/{}/{}'.format(key, info))
+
+            if info == 'stats' or info == 'seasonRank' or info == 'seasonalStats':
+                self.club[info] = g['{}'.format(key)]
+            elif info == 'info':
+                self.club[info] = g[0]
+            else:
+                self.club[info] = g
 
     def __get(self, endpoint=None):
         API_URL = 'https://www.easports.com/iframe/fifa17proclubs/api/platforms/'
-
+        
         return requests.get(API_URL+self.__plataform+endpoint).json()['raw']
 
 
 pC = ProClubs('XBOXONE')
 pC.getClub(40518)
+pprint.pprint(pC.club)
